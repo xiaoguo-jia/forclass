fb306d8b89a91817623b06678725d42984eb95b8
xiaoguo-jia <xiaoguojia@ufl.edu>
2023-03-21 17:34:23 -0400

    xiaoguo jia

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # this is xiaoguo jia
+
+def encoder(password):
+    new_pass = ''
+    for num in password:
+        new_num = int(num) + 3
+        new_pass = new_pass + str(new_num)
+    return new_pass
+
+def main():
+    password = input('Number?')
+    print(encoder(password))
+if __name__ == "__main__":
+    main()
+
