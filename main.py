b3bbd6fa9b4003dba0c857f60c5f722789385fea
gracedonath <gracedonath@ufl.edu>
2023-03-22 14:47:00 -0400

    decoder

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ def encoder(password):
         new_pass = new_pass + str(new_num)
     return new_pass
 
-def main():
-    password = input('Number?')
-    print(encoder(password))
-if __name__ == "__main__":
-    main()
+def decoder(password):
+    new_pass = ''
+    for num in password:
+        new_num = int(num) - 3
+        new_pass = new_pass + str(new_num)
+    return new_pass
 
